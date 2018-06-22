6b224c2a14e36ddbb3b7c0648882fe31dd89e95b
Sung Hwan Shim <gabriel>
2018-06-22 18:19:28 +0900

    added data for discerning main text: need more

--- getContent.js
+++ getContent.js
@@ -21,6 +21,10 @@ if(document.getElementsByTagName('p')[0]){
     document.getElementsByTagName('p')[-1].parentNode.style.border = "5px solid cyan"
 }*/
 
+//중요한 부분에 포함되어도 되지만 부모로 가지면 안되는 태그: a
+
+//아예 포함되면 안되는 태그: nav, footer, aside, header
+
 let startPoints = setStartPoints(document);
 let endPoints = [];
 
@@ -35,17 +39,22 @@ for(point of startPoints){
 }
 console.log(endPoints);
 for(point of endPoints){
-    point.style.border = "5px solid cyan";
+    if(checkParents){
+        point.style.border = "5px solid cyan";
+    }
 }
 
-function shouldIncludeParentNode(currentNode){
-    //should we go up to include another node?
+function shouldIncludeParentNode(currentNode){ //현재 노드가 중요한 본문일때 parentNode까지 중요한 본문에 포함이 되는지 확인 
+    //should we go up to include parent node?
     console.log(currentNode.parentNode.childNodes);
     for(node of currentNode.parentNode.childNodes){
-        if (node === currentNode || node.nodeType !== 1){
+        if (node === currentNode || node.nodeType !== 1){  //nodeType이 1=> element_node(p, div 태그 포함)라는 의미
             continue
         }
-        else if(isParagraph(node)){
+        if (!checkChildren(node)){ //본문에 포함되면 안되는 것들이 있는지 확인, 있으면 올라갈수 없다.
+            break;
+        }
+        else if(includeParagraph(node)){ //본문이 될 수 있는 text를 포함하는지 확인
             console.log(node);
             return true
         }
@@ -53,44 +62,71 @@ function shouldIncludeParentNode(currentNode){
     return false;
 }
 
-function isParagraph(node){
+function includeParagraph(node){
     if (node.tagName[0] === 'H' || node.tagName[0] === 'P'){
         return true;
     }
-    if(node.querySelector('p') && node.querySelector('p').textContent){
-        return true; 
+    console.log(node);
+    console.log(node.querySelectorAll('p, h1, h2, h3, h4, h5'));
+    if(node.querySelectorAll('p, h1, h2, h3, h4, h5')[0] && node.querySelectorAll('p, h1, h2, h3, h4, h5')[0].textContent){
+        return true;
     }
-    if(node.querySelector('span'))
+    if(node.querySelector('span')){
+    }
     return false;
     //return node.tagName.includes('H') || node.tagName.includes('P') || node.querySelector('p');
 }
 
-function findParagraphs(doc){
-    if (doc.querySelector('main p')) {
-        return doc.querySelectorAll('main p');
+function findParagraphs(doc){  //본문이라고 생각되는 태그들을 일차적으로 가져옴
+    let paragraphNodes = [];
+    /*if (doc.querySelector('main p')) {
+        paragraphNodes.push(doc.querySelectorAll('main p'));
     }
-    else if (doc.querySelector('[role="main"] p')) {
-        return doc.querySelectorAll('[role="main"] p');
+    if (doc.querySelector('[role="main"] p')) {
+        paragraphNodes.push(doc.querySelectorAll('[role="main"] p'));
     }
-    else if (doc.querySelector('.content p')) {
-        return doc.querySelectorAll('.content p');
+    if (doc.querySelector('.content p')) {
+        paragraphNodes.push(doc.querySelectorAll('.content p'));
     }
-    else if (doc.querySelector('article p')) {
-        return doc.querySelectorAll('article p');
+    if (doc.querySelector('article p')) {
+        paragraphNodes.push(doc.querySelectorAll('article p'));
     }
-    else if (doc.querySelector('#main p')) {
-        return doc.querySelectorAll('#main p');
+    if (doc.querySelector('#main p')) {
+        paragraphNodes.push(doc.querySelectorAll('#main p'));
         //throw new Error('noop');
-    }    
+    }*/
+    if (doc.querySelector('h1, h2, h3, h4, h5, p')){
+        paragraphNodes.push(doc.querySelectorAll('h1, h2, h3, h4, h5, p'))
+    }
+    console.log(paragraphNodes);
+    return paragraphNodes;
 }
 
-function setStartPoints(doc){
+function setStartPoints(doc){  //탐색을 시작할 가장 아랫단계의 시작점들을 선별
     points = [];
-    for(node of findParagraphs(doc)){
-        if(!points.includes(node.parentNode)){
-            points.push(node.parentNode);
+    for(nodes of findParagraphs(doc)){
+        for(node of nodes){
+            if(!points.includes(node.parentNode) && checkParents(node)){
+                points.push(node.parentNode);
+            }
         }
     }
     console.log(points);
     return points;
+}
+
+function checkParents(node){ //footer tag나 aside 태그 node를 부모로 가지고 있으면 거른다.
+    let banlist = ['FOOTER', 'ASIDE', 'A', 'HEADER', 'NAV', 'SPAN']
+    while(node){
+        if(banlist.includes(node.tagName)){
+            return false;
+        }
+        node = node.parentNode;
+    }
+    return true;
+}
+
+function checkChildren(node){ //이 node가 본문에 포함되도 상관이 없는가?
+    let banlist = ['FOOTER', 'ASIDE', 'HEADER', 'NAV', 'SPAN'];
+    return banlist.includes(node.tagName) || node.querySelectorAll('footer', 'aside', 'header', 'nav')[0];
 }
